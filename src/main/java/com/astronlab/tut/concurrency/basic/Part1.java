12d01ec52c397e88d9cde7682238e91c559e3d94
omar391 <madman123>
2016-04-06 22:04:39 +0600

    -init

---
+++ src/main/java/com/astronlab/tut/concurrency/basic/Part1.java
@@ -0,0 +1,18 @@
+package com.astronlab.tut.concurrency.basic;
+
+/*
+-  Java included high-level concurrency APIs from JDK 5.0
+
+ If a thread reads a memory location while another thread writes to it,
+ - what value will the first thread end up reading?
+ -- The old value?
+ -- The value written by the second thread? 
+ Or a value that is a mix between the two? 
+ Or, if two threads are writing to the cognate memory location simultanously, 
+ what value will be left when they are done? The value written by the first thread? 
+ The value written by the second thread? Or a mix of the two values written?
+
+ */
+public class Part1 {
+
+}
